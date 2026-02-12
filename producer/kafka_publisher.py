c83dde219a499eb2a6e169e4fd2ee6315bf0a85c
arielhalifa <arielhalifa@gmail.com>
2026-02-12 11:11:37 +0200

    kafka consumer in progress

--- producer/kafka_publisher.py
+++ producer/kafka_publisher.py
@@ -15,7 +15,7 @@ def delivery_report(err, msg):
     else:
         print(f"✅ Delivered {msg.value().decode("utf-8")}")
         print(f"✅ Delivered to {msg.topic()} : partition {msg.partition()} : at offset {msg.offset()}")
-cursor = collection.find()
+cursor = collection.find({},{"_id": 0})
 for c in cursor:
     value = json.dumps(c).encode('utf-8')
     producer.produce(
