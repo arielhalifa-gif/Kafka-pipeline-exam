acc026ed39cac4ce924bebd4803b0619a55ade19
arielhalifa <arielhalifa@gmail.com>
2026-02-12 12:55:23 +0200

    main file

--- analytics-api/connection.py
+++ analytics-api/connection.py
@@ -1,34 +1,7 @@
-from consumer.mysql_connection import cursor
-from fastapi import FastAPI
-import uvicorn
+import mysql.connector
 
-
-app = FastAPI()
-
-@app.get("/analytics/top-customers")
-def top_customers():
-    query = """SELECT customerName, salesRepEmployeeNumber FROM customers
-                SORT BY salesRepEmployeeNumber DESC
-                LIMIT 10"""
-    cursor.execute(query)
-    result = cursor.fetchall()
-    return {'top ten': result}
-
-
-@app.get("/analytics/customers-without-orders")
-def customers_without_orders():
-    query = """SELECT customerName, salesRepEmployeeNumber FROM customers
-                WHERE salesRepEmployeeNumber == null"""
-    cursor.execute(query)
-    result = cursor.fetchall()
-    return {"without-orders": result}
-
-
-@app.get("/analytics/zero-credit-active-customers")
-def zero_credit_active_customers():
-    query = """SELECT customers.customerName, orders.status, customers.creditLimit FROM customers
-                INNER JOIN orders ON customers.customerNumber = orders.customerNumber
-                WHERE customers.creditLimit = 0"""
-    cursor.execute(query)
-    result = cursor.fetchall()
-    return {"zero credits": result}
+cnx = mysql.connector.connect(user='root',
+                              password='password',
+                              host='127.0.0.1',
+                              database = 'suspicious_customers_orders')
+cursor = cnx.cursor()
